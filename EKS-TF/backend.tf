7f1373a5a7f2fc1321599159fb3564ff9a805da2
samarth2019 <53248872+samarth2019@users.noreply.github.com>
2024-07-25 20:52:14 -0500

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eks-s3-mj"       # Replace with your actual S3 bucket name
+    bucket = "jen-terra-s3"       # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-west-2"
   }
