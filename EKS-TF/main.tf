f172823f38b141f969470e7ddfc6f4285cd4a11e
samarth2019 <53248872+samarth2019@users.noreply.github.com>
2024-07-27 18:59:13 -0500

    Update main.tf

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -52,14 +52,14 @@ resource "aws_iam_role" "example1" {
   name = "eks-node-group-cloud"
 
   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [{
       Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
-        Service = "ec2.amazonaws.com"
+        Service = ["ec2.amazonaws.com"]
       }
     }]
-    Version = "2012-10-17"
   })
 }
 
